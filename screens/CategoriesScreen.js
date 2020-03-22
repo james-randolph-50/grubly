0a8316d1e4166de0f4cc579a73d780d1170aa9bb
James Randolph <james.randolph2016@gmail.com>
2020-03-22 09:26:09 -0400

    screen files added

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = props => {
+    return (
+        <View style={}>
+            <Text>The Categories Screen</Text>
+        </View>
+    )
+};
+
+const style = Stylesheet.create({});
+
+export default CategoriesScreen;
