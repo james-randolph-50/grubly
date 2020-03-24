3be76d066de2571e40ccd7b59bf565449fa99261
James Randolph <james.randolph2016@gmail.com>
2020-03-24 12:49:51 -0400

    basic screens setup

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,12 +3,18 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const CategoriesScreen = props => {
     return (
-        <View style={}>
+        <View style={styles.screen}>
             <Text>The Categories Screen</Text>
         </View>
     )
 };
 
-const style = Stylesheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
 export default CategoriesScreen;
