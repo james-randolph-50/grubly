6d529dba767f276a14a2f62d105967fc587dbad7
James Randolph <james.randolph2016@gmail.com>
2020-08-25 18:45:47 -0400

    navigation shit

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -9,6 +9,7 @@ const CategoriesScreen = props => {
       <Button title="Go to Meals" onPress={() => {
         props.navigation.navigate({routeName: 'CategoryMeals'});
       }} />
+
     </View>
   );
 };
