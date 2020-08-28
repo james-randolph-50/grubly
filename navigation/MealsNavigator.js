62757b950de5c55323f6a2b890097651f6ad7c49
James Randolph <james.randolph2016@gmail.com>
2020-08-28 07:58:40 -0400

    nav more DRY

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,3 +1,4 @@
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator} from 'react-navigation-stack';
 
@@ -5,11 +6,23 @@ import { createStackNavigator} from 'react-navigation-stack';
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
+  Categories: {
+    screen: CategoriesScreen,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor },
   CategoryMeals: {
-    screen: CategoryMealsScreen
+    screen: CategoryMealsScreen,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+    }
   },
   MealDetail: MealDetailScreen
 });
