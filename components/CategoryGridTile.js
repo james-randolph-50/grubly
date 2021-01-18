341613c6bbf0563e6f60e2f16258fdd50f985ccd
James Randolph <james.randolph2016@gmail.com>
2021-01-18 09:56:50 -0500

    styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -5,7 +5,8 @@ const CategoryGridTile = props => {
     return (<TouchableOpacity 
         style={styles.gridItem}
         onPress={props.onSelect}>
-      <View style={{backgroundColor: props.color}}>
+      <View 
+        style={{ ...styles.container, ...{ backgroundColor: props.color } }}>
         <Text>{props.title}</Text>
       </View>
     </TouchableOpacity>);
@@ -16,6 +17,15 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150
+  },
+  container:  {
+      flex: 1,
+      borderRadius: 10,
+      shadowColor: 'black',
+      shadowOpacity: .25,
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 10,
+      elevation: 3
   }
 });
 
