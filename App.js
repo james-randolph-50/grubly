0b3bc13ae7ad4d93845e598ad6df9802b8f32a30
James Randolph <james.randolph2016@gmail.com>
2021-01-18 09:52:26 -0500

    category grid tile componeot

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ useScreens();
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf') // test
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 };
 
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,22 +2,18 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { CATEGORIES } from  '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
   const renderGridItem = (itemData) => {
-    return (
-      <TouchableOpacity 
-        style={styles.gridItem}
-        onPress={() => {
-          props.navigation.navigate({routeName: 'CategoryMeals', params: {
-            categoryId: itemData.item.id
-          }});
-        }}>
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
-    </TouchableOpacity>
-    );
+    return <CategoryGridTile 
+              title={itemData.item.title} 
+              color={itemData.item.color}
+              onSelect={()  => {
+                props.navigation.navigate({routeName: 'CategoryMeals', params: {
+                  categoryId: itemData.item.id
+      }});
+    }} />;
   };  
   return (
     <FlatList 
@@ -37,11 +33,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
   }
 });
 
